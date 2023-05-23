541494d4e0474a0ee868be375353482a9b71a772
Leon Ma <1886818+leonmtg@users.noreply.github.com>
2023-05-23 17:40:02 +0800

    Initial Commit

---
+++ AnkiCardMaker/AnkiCardMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnkiCardMakerApp.swift
+//  AnkiCardMaker
+//
+//  Created by Leon on 2023/5/23.
+//
+
+import SwiftUI
+
+@main
+struct AnkiCardMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
